daa67f659820e38f12b5be686d20b2b3812e7cb5
Shreyansh Bansal <103424363+shreyansh3000@users.noreply.github.com>
2025-09-13 13:15:03 +0530

    Create Utilities.h

---
+++ Utilities.h
@@ -0,0 +1,16 @@
+#define uS_TO_S_FACTOR      1000000ULL  /* Conversion factor for micro seconds to seconds */
+#define TIME_TO_SLEEP       30          /* Time ESP32 will go to sleep (in seconds) */
+
+#define UART_BAUD           115200
+
+#define MODEM_TX            17
+#define MODEM_RX            16
+#define MODEM_PWRKEY        26
+#define MODEM_RESET         27
+
+#define SD_MISO             19
+#define SD_MOSI             23
+#define SD_SCLK             18
+#define SD_CS               5
+
+#define LED_PIN             12
